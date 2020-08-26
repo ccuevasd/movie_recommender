893f6e61b0ff9162e0445b5aba8531e55fc430b6
Camilo Cuevas <milo.cd@gmail.com>
2020-08-26 12:22:16 +0200

    Modified path on filtering.py

--- flask_app/filtering.py
+++ flask_app/filtering.py
@@ -5,10 +5,13 @@ import seaborn as sns
 import re
 
 
-df_MovGen = pd.read_csv(r'/Users/Simon/Documents/Data_Science/Spiced_Academy/Course_Work/Week__10/Project/data/MovGenRec.csv')
+df_MovGen = pd.read_csv(r'../data//MovGenRec.csv')
+
 
 def recommend_by_genre(UserInput):
-    recommendations = df_MovGen[df_MovGen[UserInput] == 1].sort_values(by='Num_Ratings', ascending=False)[:50].sort_values(by='Mean_Rating', ascending=False)[:10]['title']
+    recommendations = df_MovGen[df_MovGen[UserInput] == 1].sort_values(
+        by='Num_Ratings', ascending=False)[:50].sort_values(by='Mean_Rating', ascending=False)[:10]['title']
     return list(recommendations)
+
 
 print(recommend_by_genre('Comedy'))
